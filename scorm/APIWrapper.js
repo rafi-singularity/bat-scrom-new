2c3d3f38db776b2de80e2ccb8c67208f5bd7056c
AR <rafi.singularity@gmail.com>
2024-09-18 16:30:39 +0600

    initial commit

---
+++ scorm/APIWrapper.js
@@ -0,0 +1,152 @@
+var pipwerks = {}; // pipwerks 'namespace' to avoid conflicts
+pipwerks.SCORM = {
+    version: "1.2", // Default SCORM version
+    handleCompletionStatus: true,
+    handleExitMode: true,
+    API: {
+        handle: null,
+        isFound: false
+    },
+    connection: {
+        isActive: false
+    },
+    data: {
+        completionStatus: null,
+        exitStatus: null
+    },
+    debug: {
+        isActive: true,
+        display: function (msg) {
+            if (pipwerks.debug.isActive) {
+                console.log(msg);
+            }
+        }
+    }
+};
+
+pipwerks.SCORM.init = function () {
+    var success = false;
+    if (!pipwerks.SCORM.connection.isActive) {
+        var API = pipwerks.SCORM.API.getHandle();
+        if (API !== null) {
+            if (pipwerks.SCORM.version === "1.2") {
+                success = (API.LMSInitialize("") === "true");
+            }
+        }
+        if (success) {
+            pipwerks.SCORM.connection.isActive = true;
+            pipwerks.SCORM.data.completionStatus = pipwerks.SCORM.get("cmi.core.lesson_status");
+            if (pipwerks.SCORM.handleCompletionStatus) {
+                if (pipwerks.SCORM.data.completionStatus === "not attempted" || pipwerks.SCORM.data.completionStatus === "unknown") {
+                    pipwerks.SCORM.set("cmi.core.lesson_status", "incomplete");
+                }
+            }
+        }
+    }
+    return success;
+};
+
+pipwerks.SCORM.get = function (parameter) {
+    var value = null;
+    if (pipwerks.SCORM.connection.isActive) {
+        var API = pipwerks.SCORM.API.getHandle();
+        if (API !== null) {
+            if (pipwerks.SCORM.version === "1.2") {
+                value = API.LMSGetValue(parameter);
+            }
+        }
+    }
+    return value;
+};
+
+pipwerks.SCORM.set = function (parameter, value) {
+    var success = false;
+    if (pipwerks.SCORM.connection.isActive) {
+        var API = pipwerks.SCORM.API.getHandle();
+        if (API !== null) {
+            if (pipwerks.SCORM.version === "1.2") {
+                success = (API.LMSSetValue(parameter, value) === "true");
+            }
+        }
+    }
+    return success;
+};
+
+pipwerks.SCORM.save = function () {
+    var success = false;
+    if (pipwerks.SCORM.connection.isActive) {
+        var API = pipwerks.SCORM.API.getHandle();
+        if (API !== null) {
+            if (pipwerks.SCORM.version === "1.2") {
+                success = (API.LMSCommit("") === "true");
+            }
+        }
+    }
+    return success;
+};
+
+pipwerks.SCORM.quit = function () {
+    var success = false;
+    if (pipwerks.SCORM.connection.isActive) {
+        var API = pipwerks.SCORM.API.getHandle();
+        if (API !== null) {
+            if (pipwerks.SCORM.version === "1.2") {
+                success = (API.LMSFinish("") === "true");
+            }
+            if (success) {
+                pipwerks.SCORM.connection.isActive = false;
+            }
+        }
+    }
+    return success;
+};
+
+pipwerks.SCORM.API.getHandle = function () {
+    if (!pipwerks.SCORM.API.isFound) {
+        pipwerks.SCORM.API.find();
+    }
+    return pipwerks.SCORM.API.handle;
+};
+
+pipwerks.SCORM.API.find = function (win) {
+    win = win || window;
+    var API = null;
+    while (win.API === undefined && win.parent !== null && win.parent !== win) {
+        win = win.parent;
+    }
+    if (win.API !== undefined) {
+        API = win.API;
+    } else {
+        console.log("SCORM API not found.");
+    }
+    pipwerks.SCORM.API.handle = API;
+    pipwerks.SCORM.API.isFound = (API !== null);
+};
+
+// MCQ
+let answer = '';
+
+const selectHandler = (selected) => {
+    const options = document.querySelectorAll('.mcq');
+    options.forEach(option => {
+        option.classList.remove('correct');
+    });
+    selected.classList.add('correct');
+    answer = selected.id;
+}
+
+
+const handleSubmit = async () => {
+    if (answer !== '') {
+        if (answer === 'd') {
+            loadPage('./page-10.html');
+            return
+        } else {
+            const newDocument = await loadPage('./page-9.html');
+            newDocument.getElementById(answer).classList.add('wrong');
+            return
+        }
+    } else {
+        return;
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E72582',
+        yellow: '#FFBB00',
+        blue: '#0E2B63'
+      }
+    },
+  },
+  plugins: [],
+}
