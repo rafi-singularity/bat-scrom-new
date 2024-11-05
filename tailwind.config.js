8dd7b8686bbad501cee3879746cc9146d1bc01e5
siam <siamsolaiman.singularity@gmail.com>
2024-11-05 17:20:55 +0600

    added video, new page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#E72582',
-        yellow: '#FFBB00',
-        blue: '#0E2B63'
-      }
+        primary: "#E72582",
+        yellow: "#FFBB00",
+        blue: "#0E2B63",
+        secondary: "#EC6DAA",
+      },
     },
   },
   plugins: [],
-}
+};
