544cd0ac62467f7fa8f54fa8ba8870546d4b1d3c
siam <siamsolaiman.singularity@gmail.com>
2024-11-13 22:14:49 +0600

    scenario 7 done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      // padding: "100px",
+      screens: {
+        md: "1080px",
+        // => @media (min-width: 1280px) { ... }
+
+        lg: "1336px",
+        // => @media (min-width: 1536px) { ... }
+
+        xl: "1720px",
+        // => @media (min-width: 1745px) { ... }
+      },
+    },
     extend: {
       colors: {
-        primary: '#5A328A',
-        yellow: '#FFBB00',
-        blue: '#0E2B63'
-      }
+        primary: "#5A328A",
+        yellow: "#FFBB00",
+        blue: "#0E2B63",
+      },
+      screens: {
+        "xl-zoom": "1600px", // Approximate effective viewport at 125% zoom on 1920px
+        "lg-zoom": "1336px", // Approximate effective viewport at 125% zoom on 1440px
+      },
     },
   },
   plugins: [],
-}
+};
