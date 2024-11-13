245ceb811c6a71864a56be3ae3b547ae404da6e7
siam <siamsolaiman.singularity@gmail.com>
2024-11-13 22:51:45 +0600

    half  done of drag and drop

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      // padding: "100px",
+      screens: {
+        md: "1080px",
+        // => @media (min-width: 1280px) { ... }
+
+        lg: "1336px",
+        // => @media (min-width: 1536px) { ... }
+
+        xl: "1720px",
+        // => @media (min-width: 1745px) { ... }
+      },
+    },
     extend: {
       colors: {
         primary: "#2F912F",
         yellow: "#FFBB00",
         blue: "#0E2B63",
         secondary: "#8BBD86",
+      },
+      screens: {
+        "xl-zoom": "1600px", // Approximate effective viewport at 125% zoom on 1920px
+        "lg-zoom": "1336px", // Approximate effective viewport at 125% zoom on 1440px
       },
     },
   },
