16f04362c171626ef108f62a6c3f6d2c95fc08a4
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2024-11-13 23:02:41 +0600

    scenario 09 completion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src//*.{html,js}"],
   theme: {
+    container: {
+      // padding: "100px",
+      screens: {
+        md: "1080px",
+        // => @media (min-width: 1280px) { ... }
+
+        lg: "1336px",
+        // => @media (min-width: 1536px) { ... }
+
+        xl: "1720px",
+        // => @media (min-width: 1745px) { ... }
+      },
+    },
     extend: {
       colors: {
         primary: "#14A2DF",
+        secondary: "#32BDF8",
         yellow: "#FFBB00",
         blue: "#0E2B63",
-        secondary: "#32BDF8",
+        
+      },
+      screens: {
+        "xl-zoom": "1600px", // Approximate effective viewport at 125% zoom on 1920px
+        "lg-zoom": "1336px", // Approximate effective viewport at 125% zoom on 1440px
       },
     },
   },
-  plugins: [],
+  plugins: [],
 };
