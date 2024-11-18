92167f88bed34b4921a6df89016e518835349e43
AR <rafi.singularity@gmail.com>
2024-11-18 10:38:22 +0600

    scorm updated

--- scenario-2-scorm/scorm/APIWrapper.js
+++ scenario-2-scorm/scorm/APIWrapper.js
@@ -157,6 +157,12 @@ const handleSubmit = async () => {
         console.log(checkboxes);
         checkboxes.forEach((checkbox, index) => {
             checkbox.checked = selectedValues[index] === 0;
+            const parentDiv = checkbox.closest(".mcq");
+            if (selectedValues[index] !== answer[index]) {
+                parentDiv.classList.add("wrongBorder");
+            } else {
+                parentDiv.classList.add("rightBorder");
+            }
             console.log(checkbox.checked, selectedValues[index] === 0 ? false : true);
         })
         return selectedValues = [];
