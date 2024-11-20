6cbbf5ce1ee794c954b76362e048e21f04ca2ae6
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2024-11-21 02:05:04 +0600

    scenario 02 push

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      // padding: "100px",
+      screens: {
+        md: "980px",
+        // => @media (min-width: 1280px) { ... }
+
+        lg: "1236px",
+        // => @media (min-width: 1536px) { ... }
+
+        xl: "1620px",
+        // => @media (min-width: 1745px) { ... }
+      },
+    },
     extend: {
       colors: {
-        primary: '#2F912F',
-        yellow: '#FFBB00',
-        blue: '#0E2B63'
-      }
+        primary: "#2F912F",
+        yellow: "#FFBB00",
+        blue: "#0E2B63",
+      },
+      screens: {
+        "xl-zoom": "1600px", // Approximate effective viewport at 125% zoom on 1920px
+        "lg-zoom": "1336px", // Approximate effective viewport at 125% zoom on 1440px
+      },
     },
   },
   plugins: [],
-}
+};
